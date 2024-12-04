a0c83e3903a482f1d8d15ca54ae049cfd9ceb3e5
abhi4810 <abhishek991137@gmail.com>
2024-12-04 18:09:38 +0530

    secondcommit

---
+++ cypress/e2e/Demo4.cy.js
@@ -0,0 +1,9 @@
+describe ('cypress x path support', ()=> {
+    it ('Cypress X path',()=>
+    
+    
+    {
+        cy.visit ('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    })
+}
+)
