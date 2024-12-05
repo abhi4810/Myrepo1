c2140f4437403ccb8fe76fa614b227486a330c9b
abhi4810 <abhishek991137@gmail.com>
2024-12-05 18:59:44 +0530

    changes in demo4

--- cypress/e2e/Demo4.cy.js
+++ cypress/e2e/Demo4.cy.js
@@ -1,7 +1,5 @@
 describe ('cypress x path support', ()=> {
     it ('Cypress X path',()=>
-    
-    
     {
         cy.visit ('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     })
